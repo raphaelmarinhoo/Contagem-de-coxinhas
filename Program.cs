9ecc7ba24eb7124c6572fb201549f581b66393e2
Raphael Marinho <raphael.lukas.m@gmail.com>
2021-07-20 23:01:02 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Classe {
+    static void Main() {
+        string h, p;
+        string[] line = Console.ReadLine().Split(" ");
+        h = line[0];
+        p = line[1];
+        double media = Double.Parse(h) / Double.Parse(p);
+        Console.WriteLine(string.Format("{0:0.00}", media));
+    }
+}
+
+
+
